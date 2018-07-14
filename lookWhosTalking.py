a1fe626173ffac82b67bc7bbeac7d3eb0110db9a
James <jchalmers811@gmail.com>
2018-07-14 20:51:37 +1200

    Program can read from stdin and write to stdout

--- lookWhosTalking.py
+++ lookWhosTalking.py
@@ -6,4 +6,13 @@ __version__ = "1.0.1"
 __email__ = "jchalmers811@gmail.com"
 __status__ = "Test"
 
+import sys
 
+# for arg in sys.argv[1:]:
+#   print(arg)
+
+for line in sys.stdin:
+    print(line)
+
+
+print("done")
