b15e7b3dba69ed89cc2da8e9c32274d92ad74c0f
James <jchalmers811@gmail.com>
2018-07-14 17:20:54 +1200

    Initial commit

---
+++ lookWhosTalking.py
@@ -0,0 +1,9 @@
+__author__ = "James Chalmers"
+__copyright__ = "Copyright 2018, 326 Project"
+__credits__ = ["James Chalmers"]
+
+__version__ = "1.0.1"
+__email__ = "jchalmers811@gmail.com"
+__status__ = "Test"
+
+
